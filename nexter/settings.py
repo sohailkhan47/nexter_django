93ccbbbfeb7d225b516fda0dadf0ac04d93b0580
Sahir <pk45sohailkhan@gmail.com>
2021-05-09 11:15:48 +0500

    Uploaded listings

--- nexter/settings.py
+++ nexter/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'nexter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'nexter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nexterdb',
+        'USER': 'postgres',
+        'PASSWORD': 'django1234',
+        'HOST': 'localhost'
+
     }
 }
 
@@ -116,10 +123,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'nexter/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
---
+++ nexter/static/js/script.js
@@ -0,0 +1,66 @@
+'use strict';
+const btnOpen = document.querySelector('.open');
+const btnClose = document.querySelector('.close');
+const nav = document.querySelector('.header__nav');
+const link = document.querySelectorAll('.header__item');
+// const not_liked = document.querySelectorAll('.not_liked');
+// const liked = document.querySelectorAll('.liked');
+if (btnOpen) {
+btnOpen.addEventListener('click', function () {
+  btnOpen.classList.add('hidden');
+  btnClose.classList.remove('hidden');
+  nav.classList.remove('hidden');
+});
+
+btnClose.addEventListener('click', function () {
+  btnOpen.classList.remove('hidden');
+  btnClose.classList.add('hidden');
+  nav.classList.add('hidden');
+})};
+for (let i = 0; i < link.length; i++) {
+  link[i].addEventListener('click', function () {
+    btnClose.classList.add('hidden');
+    btnOpen.classList.remove('hidden');
+    nav.classList.add('hidden');
+  });
+}
+
+const popupBtn = document.querySelector('.popup__btn');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close');
+const body = document.querySelector('.container');
+const popUpContent = document.querySelector('.popup__content');
+
+const hidePopUp = function () {
+  popup.classList.remove('show_form');
+  body.style.overflow = 'visible';
+  popUpContent.style.transform = 'translate(-50%, -50%) scale(.5)';
+  popUpContent.style.opacity = '0';
+};
+
+if (popupBtn) {
+  popupBtn.addEventListener('click', function () {
+    popup.classList.add('show_form');
+    popUpContent.style.transform = 'translate(-50%, -50%) scale(1)';
+    popUpContent.style.opacity = '1';
+    popup.style.overflow = 'auto';
+    if (popup.classList.contains('show_form')) {
+      body.style.overflow = 'hidden';
+    }
+  });
+  popupClose.addEventListener('click', hidePopUp);
+}
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && popup.classList.contains('show_form')) {
+    hidePopUp();
+  }
+});
+
+// Testing the like icon functionality
+const likeJs = document.querySelector('.js__like');
+const liked = document.querySelector('.home__liked');
+// if (likeJs) {
+//   likeJs.addEventListener('click', function () {
+//   }
+// };
