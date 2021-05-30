c647b56c90675b2ed76394877d7080cbb06144cb
Sahir <pk45sohailkhan@gmail.com>
2021-05-30 09:35:22 +0500

    Final commit before deployment

--- nexter/settings.py
+++ nexter/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -145,3 +146,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR : 'error',
 }
+
+# Email Config
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'inforingleader203@gmail.com'
+EMAIL_HOST_PASSWORD = 'Pakistan@1'
+EMAIL_USE_TLS = True
